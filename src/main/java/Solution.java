5766c06fae30a967ba69d27e68f0024abe31dab2
alek <wait82985@gmail.com>
2023-09-02 18:52:44 +0600

    problem solved. Top down DP

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,38 @@
+import java.util.*;
+
+
 public class Solution {
     public int minExtraChar(String s, String[] dictionary) {
-        //TODO
-        return 0;
+        int leftover = s.length();
+        Integer[][][] memo = new Integer[s.length() + 1][s.length() + 1][s.length() + 1];
+        Set<String> set = new HashSet<>(Arrays.asList(dictionary));
+        return dfs(s, set, 0, 1, leftover, memo);
+    }
+
+    private int dfs(String s, Set<String> set, int left, int right, int penalty, Integer[][][] memo) {
+
+        if (right > s.length()) {
+            return penalty;
+        }
+        if(memo[left][right][penalty] != null){
+            return memo[left][right][penalty];
+        }
+        int result = Integer.MAX_VALUE;
+        int take = Integer.MAX_VALUE;
+        int expandRight= Integer.MAX_VALUE;
+        int shrinkLeft = Integer.MAX_VALUE;
+        if (set.contains(s.substring(left, right))){
+            take = dfs(s, set, right, right + 1, penalty - (right - left), memo);
+        }
+
+        expandRight = dfs(s, set, left, right + 1, penalty, memo);
+        if(right - left > 1){
+            shrinkLeft = dfs(s, set, left + 1, right, penalty, memo);
+        }
+
+        result = Math.min(take, Math.min(expandRight, shrinkLeft));
+        memo[left][right][penalty] = result;
+        return result;
+
     }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -16,10 +16,19 @@ public class SolutionTest {
     public void minExtraCharTest2() {
         String s = "sayhelloworld";
         String[] dictionary = {"hello", "world"};
-        int expected = 1;
+        int expected = 3;
         int actual = new Solution().minExtraChar(s, dictionary);
 
         Assert.assertEquals(expected, actual);
     }
 
+    @Test
+    public void minExtraCharTest3() {
+        String s = "dwmodizxvvbosxxw";
+        String[] dictionary = {"ox","lb","diz","gu","v","ksv","o","nuq","r","txhe","e","wmo","cehy","tskz","ds","kzbu"};
+        int expected = 7;
+        int actual = new Solution().minExtraChar(s, dictionary);
+
+        Assert.assertEquals(expected, actual);
+    }
 }
