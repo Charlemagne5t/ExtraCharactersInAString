207961be78e520fd321386a8d981f18edce52f37
alek <wait82985@gmail.com>
2024-09-24 14:22:18 +0500

    problem solved trie - dfs

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,38 +1,95 @@
 import java.util.*;
 
 
-public class Solution {
+class Solution {
     public int minExtraChar(String s, String[] dictionary) {
-        int leftover = s.length();
-        Integer[][][] memo = new Integer[s.length() + 1][s.length() + 1][s.length() + 1];
-        Set<String> set = new HashSet<>(Arrays.asList(dictionary));
-        return dfs(s, set, 0, 1, leftover, memo);
+        Trie trie = new Trie();
+        for(String str : dictionary) {
+            trie.insert(str);
+        }
+        int res = 0;
+        Integer[] memo = new Integer[s.length()];
+        res = dfs(s, trie, 0, memo);
+
+        return res;
+    }
+    int dfs(String s, Trie trie, int i, Integer[] memo) {
+        if(i == s.length()) {
+            return 0;
+        }
+        if(memo[i] != null) {
+            return memo[i];
+        }
+        int res = Integer.MAX_VALUE;
+        for(int j = i; j < s.length(); j++) {
+            int k = trie.search(s.substring(i, j + 1));
+            if(k == -1){
+                res = Math.min(res,  1 + dfs(s, trie, i + 1, memo));
+                break;
+            }else if(k == 0) {
+                res = Math.min(res, dfs(s, trie, j + 1, memo));
+            }else {
+                res = Math.min(res,1 + dfs(s, trie, i + 1, memo));
+
+            }
+        }
+        memo[i] = res;
+        return res;
+    }
+}
+
+class Trie {
+TrieNode root;
+
+public Trie() {
+    root = new TrieNode('0');
+}
+
+public void insert(String word) {
+    int wordLength = word.length();
+    TrieNode temp = root;
+    for (int i = 0; i < wordLength; i++) {
+        if(!temp.children.containsKey(word.charAt(i))){
+            TrieNode node = new TrieNode(word.charAt(i));
+            temp.children.put(word.charAt(i), node);
+        }
+        temp = temp.children.get(word.charAt(i));
+        if(i == wordLength - 1){
+            temp.isTerminal = true;
+        }
     }
 
-    private int dfs(String s, Set<String> set, int left, int right, int penalty, Integer[][][] memo) {
+}
 
-        if (right > s.length()) {
-            return penalty;
-        }
-        if(memo[left][right][penalty] != null){
-            return memo[left][right][penalty];
-        }
-        int result = Integer.MAX_VALUE;
-        int take = Integer.MAX_VALUE;
-        int expandRight= Integer.MAX_VALUE;
-        int shrinkLeft = Integer.MAX_VALUE;
-        if (set.contains(s.substring(left, right))){
-            take = dfs(s, set, right, right + 1, penalty - (right - left), memo);
-        }
+public int search(String word) {
+    TrieNode temp = root;
 
-        expandRight = dfs(s, set, left, right + 1, penalty, memo);
-        if(right - left > 1){
-            shrinkLeft = dfs(s, set, left + 1, right, penalty, memo);
-        }
+    for (int i = 0; i < word.length(); i++) {
+        if(temp.children.containsKey(word.charAt(i))){
+            temp = temp.children.get(word.charAt(i));
+        }else return -1;
+    }
+    return temp.isTerminal ? 0 : 1;
+}
 
-        result = Math.min(take, Math.min(expandRight, shrinkLeft));
-        memo[left][right][penalty] = result;
-        return result;
+public boolean startsWith(String prefix) {
+    TrieNode temp = root;
+    for (int i = 0; i < prefix.length(); i++) {
+        if(temp.children.containsKey(prefix.charAt(i))){
+            temp = temp.children.get(prefix.charAt(i));
+        }else return false;
+    }
+    return true;
+}
+}
 
+class TrieNode{
+    char value;
+    Map<Character, TrieNode> children = new HashMap<>();
+    boolean isTerminal;
+
+    public TrieNode(char value) {
+        this.value = value;
+        this.isTerminal = false;
     }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -31,4 +31,14 @@ public class SolutionTest {
 
         Assert.assertEquals(expected, actual);
     }
+    @Test
+    public void minExtraCharTest4() {
+        String s = "ecolloycollotkvzqpdaumuqgs";
+        String[] dictionary = {"flbri","uaaz","numy","laper","ioqyt","tkvz","ndjb","gmg","gdpbo","x","collo","vuh","qhozp","iwk","paqgn","m","mhx","jgren","qqshd","qr","qpdau","oeeuq","c","qkot","uxqvx","lhgid","vchsk","drqx","keaua","yaru","mla","shz","lby","vdxlv","xyai","lxtgl","inz","brhi","iukt","f","lbjou","vb","sz","ilkra","izwk","muqgs","gom","je"};
+        int expected = 2;
+        int actual = new Solution().minExtraChar(s, dictionary);
+
+        Assert.assertEquals(expected, actual);
+    }
+    //
 }
